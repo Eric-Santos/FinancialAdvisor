506147f4eb10eaf689d013c870a5155c843790c6
eric santos <eric355402@yahoo.com>
2021-04-01 12:32:05 -0600

    replaced home image with compressed version

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
                 <h1>Financial Advisor</h1>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
             </div>
-            <Image className="hero-image" src='/images/money.jpg' size='large' />
+            <Image className="hero-image" src='/images/min-money.jpg' size='large' />
         </div>
     )
 }

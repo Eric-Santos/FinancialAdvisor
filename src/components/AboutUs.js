49a5d02e8ca1eb6113b4730813f41fa0702e2149
eric santos <eric355402@yahoo.com>
2021-04-01 13:17:34 -0600

    fixed lighthouse ratings seo and changed images to .webp, needs logo images to render for pwa instal

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,7 +8,7 @@ const AboutUs = ()=>{
              <h1>About Us</h1>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
             </div>
-        <Image className="about-image" size='large'src='/images/thunder.jpg' fluid />
+        <Image className="about-image" size='huge' alt='thunder storm' src='/images/city.webp'  />
     </div>
     )
 }
